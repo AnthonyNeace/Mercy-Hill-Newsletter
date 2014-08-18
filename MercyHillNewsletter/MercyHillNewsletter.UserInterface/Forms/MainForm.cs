f9c54129cc33e6166482e54211f79d180d3f8f47
Anthony Neace <hyperanthony@gmail.com>
2014-08-18 00:40:30 -0400

    Refactoring parsing logic into new project
    * Refactored parsing logic out of MainForm and into
    MercyHillNewsletter.Parsing.

--- MercyHillNewsletter/MercyHillNewsletter.UserInterface/Forms/MainForm.cs
+++ MercyHillNewsletter/MercyHillNewsletter.UserInterface/Forms/MainForm.cs
@@ -11,6 +11,7 @@ using System.Collections.Specialized;
 using System.Configuration;
 using System.IO;
 using MercyHillNewsletter.Logging.Logger;
+using MercyHillNewsletter.Parsing;
 
 namespace MercyHillNewsletter.UserInterface
 {
@@ -18,10 +19,15 @@ namespace MercyHillNewsletter.UserInterface
     {
         private NameValueCollection AppSettings { get; set; }
         private TextBoxLogger _logger { get; set; }
+        private DocumentParser _parser { get; set; }
 
         public MainForm()
         {
             InitializeComponent();
+
+            _logger = new TextBoxLogger(txtLog);
+            _parser = new DocumentParser(webBrowser, _logger);
+
             statusLabel.Text = "Click Images > Create slideshow to begin.";
         }
 
@@ -47,48 +53,8 @@ namespace MercyHillNewsletter.UserInterface
 
         private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
-            // WebBrowser initialization for hiding toolbars and Mailchimp's floating bar.  
-            // Note: While we're still using the webBrowser control, we need to set the default width greater than
-            // the width of the div so that it doesn't shrink or try to float the content.
+            _parser.TakeScreenshotsOfHtmlElements();
 
-            webBrowser.ScrollBarsEnabled = false;
-            webBrowser.Document.Body.Style = "overflow:hidden";
-            webBrowser.Document.GetElementById("awesomebar").Style = "display:none";
-
-            writeToLog(string.Format("Loading web content at {0}", webBrowser.Url));
-
-            // TODO: Move this to another thread.
-
-            HtmlElementCollection elements = this.webBrowser.Document.Body.All;
-
-            int counter = 0;
-
-            writeToLog(@"Iterating HTML elements...");
-
-            foreach (HtmlElement element in elements)
-            {
-                string nameAttribute = element.GetAttribute("className");
-
-                if (!string.IsNullOrEmpty(nameAttribute) && nameAttribute == "mcnCaptionBlock")
-                {
-                    writeToLog(string.Format(@"Analyzing the {0} iterate of element {1}", counter, nameAttribute));
-
-                    webBrowser.Size = new Size(element.ClientRectangle.Width, element.ClientRectangle.Height);
-
-                    // Scroll into view
-                    element.ScrollIntoView(true);
-
-                    // Take screenshot
-                    Bitmap bitmap = new Bitmap(webBrowser.Width, webBrowser.Height);
-                    webBrowser.DrawToBitmap(bitmap, new Rectangle(0, 0, webBrowser.Width, webBrowser.Height));
-
-                    // Save screenshot to dir
-                    bitmap.Save(string.Format(@"C:\temp\imagetest-{0}-{1}.bmp", DateTime.Now.ToString("yyyyMMdd-mmHH"), counter));
-                    counter++;
-                }
-            }
-
-            writeToLog(@"HTML analysis complete.");
             txtLog.Refresh();
 
             // TODO: Use gmail API to email images to whoever needs them.
@@ -108,8 +74,6 @@ namespace MercyHillNewsletter.UserInterface
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            _logger = new TextBoxLogger(txtLog);
-
             writeToLog(@"Mercy Hill Newsletter Parser
 Track latest updates and changes at https://github.com/AnthonyNeace/Mercy-Hill-Newsletter");
         }
